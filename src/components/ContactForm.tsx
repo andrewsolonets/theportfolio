481536be6d96fdb18a661f82d4e922dfeab1e9c1
Andrew Solonets <andrewsolonets@gmail.com>
2022-11-18 21:28:56 +0000

    About and contact sections, working on form submit

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,67 @@
+export const ContactForm = () => {
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+    console.log("form!");
+  };
+  return (
+    <form
+      onSubmit={submitHandler}
+      class="flex w-1/2 flex-col gap-8"
+      action="https://getform.io/f/d384645e-fbbd-4511-bc35-a8f6094793bd"
+      method="POST"
+    >
+      <div className="relative w-full">
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          id="name"
+          class="placeholder-transparent outline-none rounded-lg border-none w-full  focus:ring-0 peer bg-primary-glass/70"
+        />
+        <label
+          htmlFor="username"
+          className="absolute left-3 -top-5 text-sm  transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base  peer-focus:-top-5 peer-focus:text-sm "
+        >
+          Name
+        </label>
+      </div>
+      <div className="relative w-full">
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          id="email"
+          class="placeholder-transparent outline-none rounded-lg border-none w-full  focus:ring-0 peer bg-primary-glass/70"
+        />
+        <label
+          htmlFor="username"
+          className="absolute left-3 -top-5 text-sm  transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base  peer-focus:-top-5 peer-focus:text-sm "
+        >
+          Email
+        </label>
+      </div>
+      <div className="relative w-full">
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Message"
+          rows={10}
+          className="w-full bg-primary-glass/70 border-none outline-none rounded-lg focus:outline-none placeholder-transparent peer "
+        />
+        <label
+          htmlFor="message"
+          className=" absolute left-3 -top-5 text-sm  transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base  peer-focus:-top-5 peer-focus:text-sm "
+        >
+          Message
+        </label>
+      </div>
+
+      <button
+        class="px-4 py-2 text-primary bg-secondary font-medium text-xl rounded-xl"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,33 +4,34 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      white: "#FFFFFF",
-      primary: {
-        DEFAULT: "#9046CF",
-        dark: "#893CCD",
-        glass: "rgb(127 50 195 / <alpha-value>)",
-      },
-      secondary: {
-        DEFAULT: "#FFD148",
-      },
-    },
-    fontFamily: {
-      sans: ["Inter", "sans-serif", ...defaultTheme.fontFamily.sans],
-      section: ["Fjalla One", "sans-serif", ...defaultTheme.fontFamily.sans],
-      heading: ["Quantico", "sans-serif", ...defaultTheme.fontFamily.sans],
-    },
     extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#FFFFFF",
+        primary: {
+          DEFAULT: "#9046CF",
+          dark: "#893CCD",
+          glass: "rgb(127 50 195 / <alpha-value>)",
+        },
+        secondary: {
+          DEFAULT: "#FFD148",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif", ...defaultTheme.fontFamily.sans],
+        section: ["Fjalla One", "sans-serif", ...defaultTheme.fontFamily.sans],
+        heading: ["Quantico", "sans-serif", ...defaultTheme.fontFamily.sans],
+      },
       dropShadow: {
         header: "0px 2px 15px rgba(0, 0, 0, 0.15)",
         card: "0px 7px 20px 2px rgba(0, 0, 0, 0.11)",
+        avatar: "0px 0px 31px 9px rgba(0, 0, 0, 0.25)",
       },
       backgroundImage: {
         bgPattern: "url('/public/assets/bgPattern.png')",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
